b00054b069846602fb9b2b122b671d9d5956cfeb
Guilherme <tec.guilherme.dsc@gmail.com>
2019-05-24 15:54:06 -0300

    Cap 7

---
+++ src/app/di/nomes-tec.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Optional } from '@angular/core';
+import { MeuLogService } from './meu-log.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NomesTecService {
+
+  constructor(@Optional() private meulog: MeuLogService) { 
+  }
+
+  getNomesTec(): string [] {
+    if (this.meulog) {
+      this.meulog.setLog('consultou o array de tecnologias');
+    }    
+    return ['Angular 2', 'TypeScript', 'JavaScript', 'HTML5', 'CSS3', 'Desenvolvimento com Angular 2'];
+  }
+}
